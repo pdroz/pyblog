7afc923ab10b34cd507668fa7bed5c25c27301a8
pdroz <tncpeterd@gmail.com>
2016-01-21 17:37:05 -0800

    Modified templates to display posts from db

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,9 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	created_data = models.DateTimeField(
+	created_date = models.DateTimeField(
 		blank = True, null=True)
-	published_data = models.DateTimeField(
+	published_date = models.DateTimeField(
 		blank=True, null=True)
 
 
