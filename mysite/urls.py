b65d7eb2b89c1c4976a6fe325c89681aa02456e9
pdroz <tncpeterd@gmail.com>
2016-03-01 19:53:58 -0800

    updated css, added static contact page, playing with links in menu

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^contact/$', TemplateView.as_view(template_name='blog/contact.html'),
+    name='contact'),
 ]
